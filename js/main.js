0aab8c9b7b031873160bb0d29cd74452da412a97
HoangVu999 <50878676+HoangVu999@users.noreply.github.com>
2020-05-13 03:37:35 +0700

    update source code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+// When the DOM is ready, run this function
 $(document).ready(function () {
-    $('#Carousel').carousel({
-        interval: 5000
-    })
+    //Set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 4000,
+    });
 });
